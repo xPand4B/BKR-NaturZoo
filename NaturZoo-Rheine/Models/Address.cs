33f33cee4cd94505d445912a05662c61ea20c215
Eric H <xpand.4beatz@gmail.com>
2019-01-15 21:28:34 +0100

    Added address model + repo

---
+++ NaturZoo-Rheine/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NaturZoo_Rheine.Models
+{
+    /// <summary>
+    /// Provides <see cref="Address"/> data.
+    /// </summary>
+    class Address : EntityBase
+    {
+        public Int32 postcode { get; set; }
+
+        public String city { get; set; }
+    }
+}
---
+++ NaturZoo-Rheine/Queries/Repositories/AddressRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using NaturZoo_Rheine.Models;
+using NaturZoo_Rheine.Queries.Core.Repositories;
+
+namespace NaturZoo_Rheine.Queries.Repositories
+{
+    /// <summary>
+    /// A <see cref="Address"/> repository for working with data in the database.
+    /// </summary>
+    class AddressRepository : GenericRepository<Address>
+    {
+        private readonly Address _address;
+        private readonly NaturZoo_Rheine.Database.Database _context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddressRepository"/> class.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"> if <paramref name="context"/> is null</exception>
+        public AddressRepository(Database.Database context) : base(context)
+        {
+            _context = context ?? throw new ArgumentNullException("context");
+            _address = new Address();
+        }
+    }
+}
